bf2f8889c53675dea475b13df3fcbc047fc696d8
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2026-03-29 10:53:20 +0200

    change/Photo/photo are uploading(problem with back was)

--- app/src/main/java/com/anspark/models/Photo.java
+++ app/src/main/java/com/anspark/models/Photo.java
@@ -1,56 +1,56 @@
-package com.anspark.models;
+    package com.anspark.models;
 
-import com.google.gson.annotations.SerializedName;
+    import com.google.gson.annotations.SerializedName;
 
-public class Photo {
-    @SerializedName("id")
-    private String id;
+    public class Photo {
+        @SerializedName("id")
+        private String id;
 
-    @SerializedName(value = "url", alternate = {"avatar_url"})
-    private String url;
+        @SerializedName(value = "url", alternate = {"avatar_url"})
+        private String url;
 
-    @SerializedName("primary")
-    private boolean primary;
+        @SerializedName("primary")
+        private boolean primary;
 
-    public Photo() {
-    }
-
-    public Photo(Photo other) {
-        if (other == null) {
-            return;
+        public Photo() {
         }
-        this.id = other.id;
-        this.url = other.url;
-        this.primary = other.primary;
-    }
 
-    public Photo(String id, String url, boolean primary) {
-        this.id = id;
-        this.url = url;
-        this.primary = primary;
-    }
+        public Photo(Photo other) {
+            if (other == null) {
+                return;
+            }
+            this.id = other.id;
+            this.url = other.url;
+            this.primary = other.primary;
+        }
 
-    public String getId() {
-        return id;
-    }
+        public Photo(String id, String url, boolean primary) {
+            this.id = id;
+            this.url = url;
+            this.primary = primary;
+        }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+        public String getId() {
+            return id;
+        }
 
-    public String getUrl() {
-        return url;
-    }
+        public void setId(String id) {
+            this.id = id;
+        }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
+        public String getUrl() {
+            return url;
+        }
 
-    public boolean isPrimary() {
-        return primary;
-    }
+        public void setUrl(String url) {
+            this.url = url;
+        }
 
-    public void setPrimary(boolean primary) {
-        this.primary = primary;
+        public boolean isPrimary() {
+            return primary;
+        }
+
+        public void setPrimary(boolean primary) {
+            this.primary = primary;
+        }
     }
-}
